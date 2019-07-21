759178ea64a68c18daab349ab6b91d7b7715cc97
Rizky Tria Meditanala <ritrime97@gmail.com>
2019-07-21 22:42:35 +0700

    postgre driver

--- application/config/database.php
+++ application/config/database.php
@@ -74,7 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'postgres://hyssizjoeomvix:f8261ae38c5ff5ed585d1014d836fdc764c5aac592fb3477517d21e680b28119@ec2-174-129-41-127.compute-1.amazonaws.com:5432/dcpr77qbrmnj1h',
 	'hostname' => 'ec2-174-129-41-127.compute-1.amazonaws.com',
 	'username' => 'hyssizjoeomvix',
 	'password' => 'f8261ae38c5ff5ed585d1014d836fdc764c5aac592fb3477517d21e680b28119',
