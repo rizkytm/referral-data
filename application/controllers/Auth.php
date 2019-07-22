de7ce7fccbb11601f190dfc6c5a94b071e873d7d
Rizky Tria Meditanala <ritrime97@gmail.com>
2019-07-22 11:43:15 +0700

    edit auth again

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -59,7 +59,7 @@ class Auth extends CI_Controller
     public function registration()
     {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user_account.email]', [
             'is_unique' => "This email has already registered!"
         ]);
         $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
